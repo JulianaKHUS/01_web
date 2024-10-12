32b697a4fdec7f0aed0c570813faded0e22b2c04
Juliana <iulianka99@mail.ru>
2024-10-12 23:17:12 +0300

    forms

--- http-server/src/main/java/ru.netology/Request.java
+++ http-server/src/main/java/ru.netology/Request.java
@@ -1,20 +1,29 @@
 package ru.netology;
 
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.net.URLEncodedUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Request {
     private final String method;
     private final String path;
     private final String headers;
     private final String body;
-public Request(String requestLine, String headers, String body) {
+    private final List<NameValuePair> queryParams;
+public Request(String requestLine, String headers, String body, List<NameValuePair> queryParams) {
     String[] parts = requestLine.split(" ");
     this.method = parts[0];
     this.path = parts[1];
     this.headers = headers;
     this.body = body;
+    this.queryParams = queryParams;
 }
 
     public String getMethod() {return method;}
@@ -25,8 +34,32 @@ public Request(String requestLine, String headers, String body) {
 
     public String getBody() {return body;}
 
+public List<NameValuePair> getQueryParam(String name) {
+return queryParams.stream()
+        .filter(p -> p.getName().equals(name))
+        .collect(Collectors.toList());
+}
+    public List<NameValuePair> getQueryParam() {
+return queryParams;
+    }
     public static Request parse(BufferedReader in) throws IOException {
     String requestLine = in.readLine();
+
+    if (requestLine == null) {
+        throw new IOException("Invalid request line");
+    }
+
+
+    var path = requestLine.split("");
+
+    if (parts.lenght !=3) {
+        throw new IOException("Invalid request line");
+
+    }
+
+    var path = parts[1];
+        var params = URLEncodedUtils.parse(path, StandardCharsets.UTF_8);
+
         StringBuilder headers = new StringBuilder();
         String line;
 
@@ -34,7 +67,7 @@ public Request(String requestLine, String headers, String body) {
            headers.append(line).append("\r\n");
         }
         String body = in.toString();
-        return new Request(requestLine, headers.toString(), body);
+        return new Request(requestLine, headers.toString(), body, params);
 
     }
 }
